98d901062a855ef53b4584c4c349adfce3e4e4d9
Jacob Kalat <kalatjacob@gmail.com>
2020-10-21 18:29:32 -0400

    Refactor project names

---
+++ test_client_UDS/cmake-build-debug/CMakeFiles/test_client_UDS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_client_UDS.dir/main.c.o"
+  "test_client_UDS"
+  "test_client_UDS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_client_UDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_server_UDS/cmake-build-debug/CMakeFiles/test_server_UDS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_server_UDS.dir/main.c.o"
+  "test_server_UDS"
+  "test_server_UDS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_server_UDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
